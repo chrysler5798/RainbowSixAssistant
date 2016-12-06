fa4e57d5e31ac89dd631cdbe7116aafb7cc57ebc
Louis <chrysler5798@hotmail.fr>
2016-12-06 18:06:27 +0100

    +Ajout de la nouvelle fonction pour voir les plans des maps + callouts +Creation de l'activite en XML + Class +Ajout de toutes les images pour cette nouvelle fonction -Clean dans le manifest, dependencies inutile //BACKUP avant import library pour add le zoom

---
+++ app/src/main/java/com/khrys/r6assistant/MapPlanActivity.java
@@ -0,0 +1,19 @@
+package com.khrys.r6assistant;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Louis on 06/12/2016.
+ * <p>
+ * RainbowSixAssistant
+ */
+
+public class MapPlanActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_mapplan);
+    }
+}
